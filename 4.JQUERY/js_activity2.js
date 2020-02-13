e0efc1f9cd7ca31530055afdf054dfa3338e4ceb
fdc-zablanandrei <47958316+fdc-zablanandrei@users.noreply.github.com>
2020-02-14 07:20:07 +0800

    2nd JS Activities added.

--- 4.JQUERY/js_activity2.js
+++ 4.JQUERY/js_activity2.js
@@ -2,30 +2,53 @@
 //JS Activity 2.1
 
 function stringLength(){
+	var longest;
+	var lgth =0;
 	var string = document.getElementById("strings").value;
-	var stringArray = str.split(" ");
+	var stringArray = string.split(" ");
+	console.log(stringArray);
+
+	for(var i=0; i < stringArray.length; i++){
+		if(stringArray[i].length > lgth){
+			var lgth = stringArray[i].length;
+			longest = stringArray[i];
+		}      
+	
+	} 
+	console.log(longest);
 }
 
+
+
+//JS Activity 2.2
 
 function isPrime(){
-	
+	var nonprime=0;
 	var num = parseInt(document.getElementById("number").value);
-
-
-	for(var i = 1; i <= num; i++){
+	for(var i = 2; i < num; i++){
 		
 		if(num % i === 0){
-			console.log(num +' is not prime');
-	}
+			nonprime = 1;
+			break;
+		}
 
+	}
+	if (num==1){
+		console.log(num + ' is not prime')
+	}	
+	else if (nonprime ==0){
+		console.log(num + ' is prime')
+	} else {
+		console.log(num+ ' is not prime')
 	}
 }
 
-
+//JS Activity 2.3
 
 function hourToMin(){
 	var min;
 	var hour = parseInt(document.getElementById("hour").value);
 	min = hour * 60;
+	console.log(min);
 	document.getElementById("mins").textContent = "It'll take "+min +" minutes";
 }
