2bc9f2fb080d82194377c0b9d6ad2e52d738ff93
fdc-zablanandrei <47958316+fdc-zablanandrei@users.noreply.github.com>
2020-02-21 17:39:44 +0800

    js activity 3 - applied validations

--- 4.JQUERY/js_activity3.js
+++ 4.JQUERY/js_activity3.js
@@ -2,45 +2,42 @@
 
 
 $('#form1').on('submit', nameAndAge);
+
+//Declared an empty array
 let nameList = [];
 function nameAndAge(e){
 
-
-//Declared an empty array
-
 //To let the output retain
-e.preventDefault();
+	e.preventDefault();
+
+//Execute Form Validation Before executing
+		if (checkForm1()==true){
+
 //Declaring Properties for the object
-let personList = {
-    Name: document.getElementById('userName').value,
-    Age: parseInt(document.getElementById('userAge').value)
-}
+		let personList = {
+			Name: document.getElementById('userName').value,
+			Age: document.getElementById('userAge').value
+		}
 
-//Form validations
-var input = document.getElementById('form1');
+//Duplicate Checking
+		if(checkDuplicate(personList.Name, nameList)==true){
+//To pass the object into the Array
+		nameList.push(personList);
+		document.querySelector('form').reset();
 
-
-if(checkForm(input)){
-    alert("Error: Input is empty!");
-}else {
-
-    //To pass the object into the Array
-    nameList.push(personList);
-    document.querySelector('form').reset();
-
-    console.log('added' , {nameList} );
-    let pre = document.querySelector('#msg pre');
-    pre.innerHTML=JSON.stringify(nameList);
+		console.log('added' , {nameList} );			
+		let pre = document.querySelector('#msg pre');
+		pre.innerHTML=JSON.stringify(nameList);
 
 
-    var text = "";
-    let readable = document.querySelector('#read pre');
-    for (let x = 0; x<nameList.length; x++){
-
-        text+=`Name:${nameList[x].Name} , Age: ${nameList[x].Age} ` + '\n';
-    }
-    readable.innerText=(text);
-}
+		var text = "";
+		let readable = document.querySelector('#read pre');
+			for (let x = 0; x<nameList.length; x++){
+				text+=`Name:${nameList[x].Name} , Age: ${nameList[x].Age} ` + '\n';
+			}
+		readable.innerText=(text);
+		}
+	}
 }
 
 //JS activity 3.2
@@ -51,82 +48,114 @@ let inventoryList = [];
 function itemInventory(e){
 
 //To let the output retain
-e.preventDefault();
+	e.preventDefault();
+
+//Execute Form Validation Before executing
+	if (checkForm2()==true){
+
 //Declaring Properties for the object
-let itemList = {
-    Item_Name: document.getElementById('itemName').value,
-    Stock: parseInt(document.getElementById('itemStock').value),
-    Price: parseFloat(document.getElementById('itemPrice').value)
-}
+		let itemList = {
+			Name: document.getElementById('itemName').value,
+			Stock: parseInt(document.getElementById('itemStock').value),
+			Price: document.getElementById('itemPrice').value
+		}
 
-//Form validations
-var input = document.getElementById('form2');
-console.log(input);
-if(checkForm(input)){
-    alert("Error: Input is empty!");
-}else {
-
+		if(checkDuplicate(itemList.Name, inventoryList)==true){
 //To pass the objects into the Array
-inventoryList.push(itemList);
-document.querySelector('form').reset();
+		inventoryList.push(itemList);
+		document.querySelector('form').reset();
 
-console.log('added' , {inventoryList} );
-var text ="";
-let pre = document.querySelector('#list pre')
+		console.log('added' , {inventoryList} );
+		var text ="";
+		let pre = document.querySelector('#list pre')
 
 //Loop in order to display the Added entries of the list
-for (let x = 0; x<inventoryList.length; x++){
-    text+=`Item Name: ${inventoryList[x].Item_Name} , Stock: ${inventoryList[x].Stock}, Price: ${inventoryList[x].Price} `+ '\n';
-}
+			for (let x = 0; x<inventoryList.length; x++){
+				text+=`Item Name: ${inventoryList[x].Name} , Stock: ${inventoryList[x].Stock}, Price: ${inventoryList[x].Price} `+ '\n';
+			}
 
-pre.innerText = (text);
-var totalPrice=0;
-var text2="";
-let tally = document.querySelector('#tally pre')
+		pre.innerText = (text);
+		var totalPrice=0;
+		var text2="";
+		let tally = document.querySelector('#tally pre')
 
 //Function for the Calculate button 
-document.getElementById('calculate').onclick = function(){
+		document.getElementById('calculate').onclick = function(){
 
-    for( var x in inventoryList){
+			for( var x in inventoryList){
 
-        product = (inventoryList[x].Stock)*(inventoryList[x].Price);
-        totalPrice += product;
-        text2 += `${inventoryList[x].Item_Name} will have a total value of ${product}.` + '\n';
+				product = (inventoryList[x].Stock)*(inventoryList[x].Price);
+				totalPrice += product;
+				text2 += `${inventoryList[x].Name} will have a total value of ${product}.` + '\n';
 
-    }
-    tally.innerText = (text2 + '\n' + 'Total Price for the items is Php' + totalPrice + `.`);
+			}
+			tally.innerText = (text2 + '\n' + 'Total Price for the items is Php' + totalPrice + `.`);
+		}	
+	}
 }
 }
-}
-
 //FORM VALIDATION
-/*function checkForm(param){
-    // validation fails if the input is blank
-    for(var i = 0; i < param.length - 1; i++){   
-        //console.log(param);
-        if(param.elements[i].value == ""){
-            console.log("empty here");
-            break;
-            
-            // return true;
-            // break;
-        }else{
-            // validation was successful
-            // return false;
-            console.log(param.elements[i].value);
-        }
-    }
-}*/
-function checkForm(param){
-    var emp = "";
 
-    for(var i = 0; i < param.length-1; i++){ //loops the entire form fields and checks if there are empty values
-        if(param.elements[i].value === ""){
-            emp = true; //at least 1 empty field is found
-            break;
-        }else{
-            emp = false; // field is not empty
-        }
-    }
-    return emp;
+
+function checkForm1(){
+	var name = document.getElementById("userName").value;
+	var age = document.getElementById("userAge").value;
+	if (name == null || name == ""){
+		alert("Error in Input ");
+		return false;
+	} else if ((name != null || name != "") && (isNaN(age) || age == null || age=="" )) {
+		alert("Error: No Input in Age");
+		return false;
+	} else if (age % 1 !=0 ) {
+		alert("Error: Age should be positive whole number");
+		return false;
+	} else if (age < 1) {
+		alert("Error: Age should be positive integer");
+		return false;
+	} else if ((name != null || name != "") && (!isNaN(age) || age != null)) {
+		return true;
+	}
 }
+
+		//validation function for form 2
+		function checkForm2() {
+			var name = document.getElementById("itemName").value;
+			var stocks = document.getElementById("itemStock").value;
+			var price = document.getElementById("itemPrice").value;
+
+			if (name === null || name === ""){
+				alert("Please Input name in product inventory");
+				return false;
+			}else if ( isNaN(stocks) || stocks == "") {
+				alert("Stocks must have a value");
+				return false;
+			} else if (stocks < 1) {
+				alert("Stocks must be greater than 0");
+				return false;
+			} else if (stocks % 1 != 0) {
+				alert("Stocks must whole number");
+				return false;
+			} else if ( isNaN(price)) {
+				alert("Price must have a value");
+				return false;
+			} else if (price < 1) {
+				alert("Stocks must be greater than 0");
+				return false;
+			} 
+			return true;
+		}
+
+//Checking of Duplicate Names
+		function checkDuplicate(listName, arrayList){
+
+			for(var i=0; i<arrayList.length; i++){
+				console.log(listName);
+					console.log(arrayList);
+				if (arrayList[i].Name === listName){
+					alert(listName + " already Exists. This is a Duplicate");
+					return false;
+				}
+			}
+			return true;
+		}
+
